387fb93626008f45316c788ff7f8ba9ef182420a
oeduardosg <eduardosg.my@gmail.com>
2024-12-06 08:31:37 -0300

    Quickfix item.h

--- lab03/exe01/item.h
+++ lab03/exe01/item.h
@@ -1,6 +1,11 @@
+#ifndef _item_h_
+#define _item_h_
+
 typedef int Item;
 
 #define key(A) (A)
 #define less(A, B) (key(A) < key(B))
 #define exch(A, B) { Item t = A; A = B; B = t; }
 #define compexch(A, B) if (less(B, A)) exch(A, B)
+
+#endif
