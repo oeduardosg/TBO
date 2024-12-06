8823af4c8aabe430d3b0fb8c542da8fe4b79d7e8
oeduardosg <eduardosg.my@gmail.com>
2024-12-06 12:43:16 -0300

    Fix mains and add time records

--- lab03/exe05/bubble_sort.c
+++ lab03/exe05/bubble_sort.c
@@ -3,9 +3,17 @@
 void sort(Item  * a, int lo, int hi) {
 
     for(int i = lo; i < hi; i++) {
+
+        int swapped = 0;
+
         for(int j = lo; j < hi - i; j++) {
-            if(less(a[j + 1], a[j])) exch(a[j], a[j + 1]);
+            if(less(a[j + 1], a[j])) {
+                exch(a[j], a[j + 1]);
+                swapped = 1;
+            }
         }
+
+        if(!swapped) break;
     }
 
 }
--- lab03/exe06/shaker_sort.c
+++ lab03/exe06/shaker_sort.c
@@ -5,17 +5,28 @@ void sort(Item  * a, int lo, int hi) {
     int turn = 1;
 
     for(int i = lo; i < hi; i++) {
+
+        int swapped = 0;
+
         if(turn) {
             for(int j = lo; j < hi - i - 1; j++) {
-                if(less(a[j + 1], a[j])) exch(a[j], a[j + 1]);
+                if(less(a[j + 1], a[j])) {
+                    exch(a[j], a[j + 1]);
+                    swapped = 1;
+                }
             }
             turn = 0;
         }
         else {
-            for(int j = hi - i; j > i + 1; j--) {
-                if(less(a[j - 1], a[j])) exch(a[j - 1], a[j]);
+            for(int j = hi; j > i + 1; j--) {
+                if(less(a[j], a[j - 1])) {
+                    exch(a[j - 1], a[j]);
+                    swapped = 1;
+                }
             }
             turn = 1;
         }
+
+        if(!swapped) break;
     }
 }
